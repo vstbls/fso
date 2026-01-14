bd33bdc83ba7e6b6de7a0b8688d10e40814157ba
vstbls <7550178+vstbls@users.noreply.github.com>
2026-01-14 18:33:04 +0200

    exercise 1.10

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,27 @@
 import { useState } from 'react'
 
+const Button = ({callback, text}) => <button onClick={callback}>{text}</button>
+
+const StatisticLine = ({text, value}) => <p>{text} {value}</p>
+
+const Statistics = ({good, neutral, bad}) => {
+  const total = good+neutral+bad
+
+  if (total === 0) return <p>No feedback given</p>
+
+  return (
+    <div>
+      <h1>Stats!</h1>
+      <StatisticLine text={':)'} value={good} />
+      <StatisticLine text={':l'} value={neutral} />
+      <StatisticLine text={':('} value={bad} />
+      <StatisticLine text={'Total feedback'} value={total} />
+      <StatisticLine text={'Average'} value={(good-bad)/total || 0} />
+      <StatisticLine text={'Positive'} value={good/total*100 || 0} />
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -11,23 +33,11 @@ const App = () => {
     <div>
       <h1>Give feedback!</h1>
       <p>
-        <button onClick={() => setGood(good+1)}>
-          Good!
-        </button>
-        <button onClick={() => setNeutral(neutral+1)}>
-          Neutral..
-        </button>
-        <button onClick={() => setBad(bad+1)}>
-          Bad...........
-        </button>
+        <Button callback={() => setGood(good+1)} text={'Good!'} />
+        <Button callback={() => setNeutral(neutral+1)} text={'Neutral..'} />
+        <Button callback={() => setBad(bad+1)} text={'Bad...........'} />
       </p>
-      <h1>Stats!</h1>
-      <p>:) {good}</p>
-      <p>:l {neutral}</p>
-      <p>:( {bad}</p>
-      <p>Total feedback: {total}</p>
-      <p>Average: {(good-bad)/total || 0}</p>
-      <p>Positive: {good/total*100 || 0}%</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }

fc7640179df4a2e68a1d9f9c7a097b0bc147b64a
vstbls <7550178+vstbls@users.noreply.github.com>
2026-01-25 15:58:55 +0200

    ex 2.9

--- part2/puhelinluettelo/src/App.jsx
+++ part2/puhelinluettelo/src/App.jsx
@@ -3,10 +3,14 @@ import Person from './Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1231244' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [phoneFilter, setPhoneFilter] = useState('')
 
   const addNumber = (event) => {
     event.preventDefault()
@@ -25,9 +29,17 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setPhoneFilter(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown results: <input value={phoneFilter} onChange={handleFilterChange} />
+      </div>
+      <h2>Add entry</h2>
       <form onSubmit={addNumber}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -40,7 +52,11 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <Person person={person} key={person.name} />)}
+      {
+        persons
+        .filter(person => person.name.toLowerCase().includes(phoneFilter.toLowerCase()))
+        .map(person => <Person person={person} key={person.name} />)
+        }
     </div>
   )
 

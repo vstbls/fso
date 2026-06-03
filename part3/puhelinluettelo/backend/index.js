ed56d947a9bb8665f00b0a2bbb3f4fe57c376dc5
vstbls <7550178+vstbls@users.noreply.github.com>
2026-06-03 15:39:33 +0300

    ex. 3.10 (?)

--- part3/puhelinluettelo/backend/index.js
+++ part3/puhelinluettelo/backend/index.js
@@ -89,7 +89,7 @@ app.post('/api/persons', (request, response) => {
     response.json(newPerson)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`SERVER RUNNING ON PORT ${PORT}`)
 })

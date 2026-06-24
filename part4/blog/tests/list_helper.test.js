66b1c7d175e250eb5f3811706f6f09e544445cd9
vstbls <7550178+vstbls@users.noreply.github.com>
2026-06-24 19:48:47 +0300

    ex 4.5

--- part4/blog/tests/list_helper.test.js
+++ part4/blog/tests/list_helper.test.js
@@ -8,3 +8,63 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
 })
+
+describe('total likes', () => {
+  blog1 = {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+  }
+
+  blog2 = {
+    title: 'Go To Statement Considered HELPFUL',
+    author: 'Evil W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Super_Helpful.html',
+    likes: 50,
+  }
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes([blog1])
+    assert.strictEqual(result, 5)
+  })
+
+  test('likes of two blogs are summed correctly', () => {
+    const result = listHelper.totalLikes([blog1, blog2])
+    assert.strictEqual(result, 55)
+  })
+
+  test('empty array has zero likes', () => {
+    assert.strictEqual(listHelper.totalLikes([]), 0)
+  })
+})
+
+describe('favorite blog', () => {
+  blog1 = {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+  }
+
+  blog2 = {
+    title: 'Go To Statement Considered HELPFUL',
+    author: 'Evil W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Super_Helpful.html',
+    likes: 50,
+  }
+
+  test('the only blog is the best blog', () => {
+    const result = listHelper.favoriteBlog([blog1])
+    assert.deepStrictEqual(result, blog1)
+  })
+
+  test('favorite from two blogs found correctly', () => {
+    const result = listHelper.favoriteBlog([blog1, blog2])
+    assert.deepStrictEqual(result, blog2)
+  })
+
+  test('empty array has no favorite', () => {
+    assert.deepStrictEqual(listHelper.favoriteBlog([]), undefined)
+  })
+})
--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,5 +1,21 @@
 const dummy = (blogs) => 1
 
+const totalLikes = (blogs) => {
+    return blogs.length === 0
+    ? 0  
+    : blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+    return blogs.length === 0
+    ? undefined
+    : blogs.reduce((best, blog) => {
+        return blog.likes > best.likes ? blog : best
+    }, blogs[0])
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }

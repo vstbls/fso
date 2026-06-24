c2dedd84ec5ecc1b622b095eaae16f5a0473dd8b
vstbls <7550178+vstbls@users.noreply.github.com>
2026-06-24 20:17:36 +0300

    ex 4.6-4.7

--- part4/blog/tests/list_helper.test.js
+++ part4/blog/tests/list_helper.test.js
@@ -9,21 +9,28 @@ test('dummy returns one', () => {
   assert.strictEqual(result, 1)
 })
 
+blog1 = {
+  title: 'Go To Statement Considered Harmful',
+  author: 'Edsger W. Dijkstra',
+  url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+  likes: 5,
+}
+
+blog2 = {
+  title: 'Go To Statement Considered HELPFUL',
+  author: 'Evil W. Dijkstra',
+  url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Super_Helpful.html',
+  likes: 50,
+}
+
+blog3 = {
+  title: 'Go To Statement Considered More Controversial Than Expected',
+  author: 'Edsger W. Dijkstra',
+  url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+  likes: 1,
+}
+
 describe('total likes', () => {
-  blog1 = {
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-  }
-
-  blog2 = {
-    title: 'Go To Statement Considered HELPFUL',
-    author: 'Evil W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Super_Helpful.html',
-    likes: 50,
-  }
-
   test('when list has only one blog equals the likes of that', () => {
     const result = listHelper.totalLikes([blog1])
     assert.strictEqual(result, 5)
@@ -40,19 +47,6 @@ describe('total likes', () => {
 })
 
 describe('favorite blog', () => {
-  blog1 = {
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-  }
-
-  blog2 = {
-    title: 'Go To Statement Considered HELPFUL',
-    author: 'Evil W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Super_Helpful.html',
-    likes: 50,
-  }
 
   test('the only blog is the best blog', () => {
     const result = listHelper.favoriteBlog([blog1])
@@ -66,5 +60,59 @@ describe('favorite blog', () => {
 
   test('empty array has no favorite', () => {
     assert.deepStrictEqual(listHelper.favoriteBlog([]), undefined)
+  })
+})
+
+describe('most blogs', () => {
+  test('the only blogger writes the most', () => {
+    assert.deepStrictEqual(listHelper.mostBlogs([blog1]), {
+      author: 'Edsger W. Dijkstra',
+      blogs: 1
+    })
+  })
+
+  test('most frequent blogger is the first one', () => {
+    assert.deepStrictEqual(listHelper.mostBlogs([blog1, blog2]), {
+      author: 'Edsger W. Dijkstra',
+      blogs: 1
+    })
+  })
+
+  test('most blogs found from three', () => {
+    assert.deepStrictEqual(listHelper.mostBlogs([blog1, blog2, blog3]), {
+      author: 'Edsger W. Dijkstra',
+      blogs: 2
+    })
+  })
+
+  test('most blogs in an empty list is undefined', () => {
+    assert.deepStrictEqual(listHelper.mostBlogs([]), undefined)
+  })
+})
+
+describe('most likes', () => {
+  test('the only blog has the most likes', () => {
+    assert.deepStrictEqual(listHelper.mostLikes([blog1]), {
+      author: 'Edsger W. Dijkstra',
+      likes: 5
+    })
+  })
+
+  test('likes are summed correctly when finding most liked', () => {
+    assert.deepStrictEqual(listHelper.mostLikes([blog1, blog3]), {
+      author: 'Edsger W. Dijkstra',
+      likes: 6
+    })
+  })
+
+  test('most blogs found from three', () => {
+    assert.deepStrictEqual(listHelper.mostLikes([blog1, blog2, blog3]), {
+      author: 'Evil W. Dijkstra',
+      likes: 50
+    })
+  })
+
+  test('most liked in an empty list is undefined', () => {
+    assert.deepStrictEqual(listHelper.mostLikes([]), undefined)
   })
 })
--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -14,8 +14,38 @@ const favoriteBlog = (blogs) => {
     }, blogs[0])
 }
 
+const mostBlogs = (blogs) => {
+    if (blogs.length === 0) return undefined
+    blog_count = blogs.reduce((dict, blog) => {
+        if (!dict[blog.author]) {
+            dict[blog.author] = 0
+        }
+        dict[blog.author] += 1
+        return dict
+    }, {})
+    unstoppable_author = Object.entries(blog_count)
+        .reduce((max, cur) => cur[1] > max[1] ? cur : max, ["", -Infinity])
+    return { author: unstoppable_author[0], blogs: unstoppable_author[1] }
+}
+
+const mostLikes = (blogs) => {
+    if (blogs.length === 0) return undefined
+    blog_count = blogs.reduce((dict, blog) => {
+        if (!dict[blog.author]) {
+            dict[blog.author] = 0
+        }
+        dict[blog.author] += blog.likes
+        return dict
+    }, {})
+    favorite_author = Object.entries(blog_count)
+        .reduce((max, cur) => cur[1] > max[1] ? cur : max, ["", -Infinity])
+    return { author: favorite_author[0], likes: favorite_author[1] }
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
